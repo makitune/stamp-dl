9d42d1848c7af4bba5357ec6bc0b45b5c629596f
makitune <msk.kawano@gmail.com>
2019-09-29 19:23:24 +0900

    Remove functions no longer used

--- fill.go
+++ fill.go
@@ -4,37 +4,7 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
-
-	"github.com/kettek/apng"
 )
-
-func (si *LineStickerImage) fillPNGBackground(clr color.Color) LineStickerImage {
-	img := si.raw.(image.Image)
-	out := filledImage(img, clr)
-	return LineStickerImage{
-		Type: si.Type,
-		raw:  out,
-	}
-}
-
-func (si *LineStickerImage) fillAPNGBackground(clr color.Color) LineStickerImage {
-	imgs := si.raw.(apng.APNG)
-	frames := []apng.Frame{}
-	for _, frame := range imgs.Frames {
-		out := filledImage(frame.Image, clr)
-		img := frame
-		img.Image = out
-		frames = append(frames, img)
-	}
-
-	return LineStickerImage{
-		Type: si.Type,
-		raw: apng.APNG{
-			Frames:    frames,
-			LoopCount: 0,
-		},
-	}
-}
 
 func filledImage(img image.Image, clr color.Color) image.Image {
 	b := img.Bounds()
--- lineStamp.go
+++ lineStamp.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"errors"
-	"image"
-	"image/color"
-	"image/png"
-	"io"
 	"strings"
-
-	"github.com/kettek/apng"
 )
 
 var lineStore = "https://store.line.me/stickershop"
@@ -24,47 +17,6 @@ const (
 	LineStickerCustom
 	LineStickerUnkown
 )
-
-// LineSticker is an object for a Line stamp image
-type LineSticker struct {
-	ID    string
-	Image LineStickerImage
-}
-
-type LineStickerImage struct {
-	Type LineStickerType
-	raw  interface{}
-}
-
-// StoreName is the sticker name for saving
-func (s *LineSticker) StoreName() string {
-	return s.ID + ".png"
-}
-
-// FilledBackgroundImage is the sticker image for saving
-func (s *LineSticker) FilledBackgroundImage(clr color.Color) (StickerImage, error) {
-	switch s.Image.Type {
-	case LineStickerStatic, LineStickerCustom:
-		si := s.Image.fillPNGBackground(color.RGBA{255, 255, 255, 255})
-		return &si, nil
-	case LineStickerAnimation:
-		si := s.Image.fillAPNGBackground(color.RGBA{255, 255, 255, 255})
-		return &si, nil
-	}
-
-	return nil, errors.New("対応していません")
-}
-
-func (si *LineStickerImage) Encode(w io.Writer) error {
-	switch si.Type {
-	case LineStickerStatic, LineStickerCustom:
-		return png.Encode(w, si.raw.(image.Image))
-	case LineStickerAnimation:
-		return apng.Encode(w, si.raw.(apng.APNG))
-	}
-
-	return errors.New("対応していません")
-}
 
 // LineStamp is a collection object for LineSticker
 type LineStamp struct {
